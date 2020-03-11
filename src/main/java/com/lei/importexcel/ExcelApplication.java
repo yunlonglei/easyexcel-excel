00953caaa1e663e10b8f1c97669ba6bba5f97aad
leiyunlong <1175333149@qq.com>
2020-03-11 09:58:52 +0800

    更新README，修改项目名称

--- src/main/java/com/lei/importexcel/ExcelApplication.java
+++ src/main/java/com/lei/importexcel/ExcelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImportExcelApplication {
+public class ExcelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ImportExcelApplication.class, args);
+        SpringApplication.run(ExcelApplication.class, args);
     }
 
 }
